f9a7024a6e416ae14e6d2808fb4cbee105c48c08
Fourcat91 <100085442+Fourcat91@users.noreply.github.com>
2022-03-07 14:14:05 +0200

    Create script.js
    test

---
+++ script.js
@@ -0,0 +1,14 @@
+const Login = [13, 23, 324, 242, 86, 76]
+let totalCart = 0;
+
+for(let i = 0; i < cart.length; i += 1) {
+
+    totalCart += cart[i];
+}
+
+console.log(totalCart);
+
+for (const car of cart) {
+    totalCart += car;
+}
+console.log(totalCart);
